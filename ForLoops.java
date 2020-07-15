e39d581b842c5e8e06a450aea0ed017d030ffb6b
Shweta <shwetagoyal2811@gmail.com>
2020-07-15 21:45:30 +0530

    forLoopCommit

--- ForLoops.java
+++ ForLoops.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class ForLoops {
+
+    public static void main(String[] args) {
+
+        System.out.println("Information about for loops!!");
+        System.out.println("let's see the changes");
+
+    }
+
 }
+
